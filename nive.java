2b453cf152af4d7116acd873b9b351489da7d64e
nivethagokul <umagovindan22@gmail.com>
2016-07-20 10:57:10 +0530

    ggg
    fff

---
+++ nive.java
@@ -0,0 +1,25 @@
+public class nn {
+
+	public static void main(String[] args) {
+		int x;
+		x=Integer.parseInt(args[0]);
+		if(x>0)
+		{
+			System.out.println("the no.is positive");
+		}
+		else if(x==0)
+		{
+			System.out.println("the no.is neither positive nor negative");
+			
+		}
+		else
+		{
+			System.out.println("the no.is negative");
+		}
+		
+	}
+
+	
+	}
+
+
